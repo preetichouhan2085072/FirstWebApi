1b99a5fcd3afe9ad2575ec7a7fbb80034bebcc91
Chouhan <2085072@cognizant.com>
2022-03-11 15:16:22 +0530

    Hello

---
+++ FirstWebApi/Controllers/EmployeeApiController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstWebApi.Models;
+
+namespace FirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeApiController : ControllerBase
+    {
+        [HttpGet]
+        public List<Emp> GetEmployees()
+        {
+            List<Emp> empList = new List<Emp>() {
+                    new Emp() { Empno = 1005, Ename = "Scott", Job = "Manager" },
+                    new Emp() { Empno = 1006, Ename = "Smith", Job = "Lead" },
+                    new Emp() { Empno = 1007, Ename = "Sandy", Job = "Programmer" },
+                    new Emp() { Empno = 1008, Ename = "Sam", Job = "Lead" }
+            };
+
+            return empList;
+        }
+
+        [HttpGet("{id}")]
+        public Emp GetEmployeeById(int id)
+        {
+            List<Emp> empList = new List<Emp>() {
+
+                    new Emp() { Empno = 1005, Ename = "Scott", Job = "Manager" },
+                    new Emp() { Empno = 1006, Ename = "Smith", Job = "Lead" },
+                    new Emp() { Empno = 1007, Ename = "Sandy", Job = "Programmer" },
+                    new Emp() { Empno = 1008, Ename = "Sam", Job = "Lead" }
+            };
+
+            Emp obj = empList.SingleOrDefault(item => item.Empno == id);
+            return obj;
+        }
+
+    }
+}
---
+++ FirstWebApi/Models/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApi.Models
+{
+    public class Emp
+    {
+        public int Empno { get; set; }
+        public string Ename { get; set; }
+        public string Job { get; set; }
+
+    }
+}
